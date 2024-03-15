3a74b0ee744e23e51a28f61ef2464fe43c0b1411
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2024-03-15 10:17:53 -0700

    feat: structured the project layout

--- solution/src/App.tsx
+++ solution/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
-import ReactInterviewForm from './components/ReactInterviewForm';
+import React from "react";
+import "./App.css";
+import ReactInterviewForm from "./components/ReactInterviewForm";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
+  /**
+   * We can wrap the MainLayout component with some providers 
+   * such as Context provider, Theme provider, etc. 
+   */
   return (
-    <ReactInterviewForm />
+    <MainLayout>
+      <ReactInterviewForm />
+    </MainLayout>
   );
 }
 
