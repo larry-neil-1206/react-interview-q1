9191b4a0847bf033bfa9c183b51a7c72f1e2da70
larry-neil-1206 <larryneilmcgowen1206@gmail.com>
2024-03-15 09:56:15 -0700

    chore: clean default app page and add a simple form component

---
+++ solution/src/components/ReactInterviewForm/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const ReactInterviewForm = () => {
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+
+  return (
+    <form className="m-5 p-5">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="name"
+        >
+          Name:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="location"
+        >
+          Location:
+        </label>
+        <select
+          className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        >
+          <option value="">--Please choose an option--</option>
+          <option value="location1">Location 1</option>
+          <option value="location2">Location 2</option>
+          <option value="location3">Location 3</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default ReactInterviewForm;
